d14880da3454e6bd18fb2fcdf73bb078cafff48b
pablobiosca <zaslake@gmail.com>
2025-02-04 19:12:02 +0100

    feat(mongo): create mongo connection & disconect

---
+++ config/dbClient.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+
+class mongoNewsClient {
+
+    constructor(){
+        this.connnectMongo()
+    }
+
+    async connnectMongo(){
+
+        try {
+            const query_string_connection = process.env.MONGO_URI
+            await mongoose.connect(query_string_connection)
+            console.log("connected to mongol")
+        } catch (e) {
+            console.log(e.error)
+        }
+
+    }
+
+    async closeConnection() {
+        try {
+            await mongoose.connection.close()
+            console.log("connection to mongo closed")
+        } catch (e) {
+            console.log(e.error)
+        }
+    }
+}
+
+export default new mongoNewsClient()
